d844fdc6e1acf7a79d6b3ab290a6f9b0b673e24e
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-11-12 19:16:46 +0000

    Added exception to handle empty api responses

---
+++ Orchestrator/src/java/com/zer0p1us/core/misc/NoDataException.java
@@ -0,0 +1,11 @@
+package com.zer0p1us.core.misc;
+
+/**
+ *
+ * @author zer0p1us
+ */
+public class NoDataException extends Exception {
+    public NoDataException(String message) {
+        super(message);
+    }
+}
