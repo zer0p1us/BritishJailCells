ff5ff523294bb92eb00b5416edc23a94d263b4a1
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-11-13 19:25:39 +0000

    Added ProjectOSRM data storage

---
+++ Orchestrator/src/java/com/zer0p1us/endpoints/models/ProjectOSRM/ProjectOSRM.java
@@ -0,0 +1,9 @@
+package com.zer0p1us.endpoints.models.ProjectOSRM;
+
+/**
+ *
+ * @author zer0p1us
+ */
+public class ProjectOSRM {
+    public Route[] routes;
+}
---
+++ Orchestrator/src/java/com/zer0p1us/endpoints/models/ProjectOSRM/Route.java
@@ -0,0 +1,10 @@
+package com.zer0p1us.endpoints.models.ProjectOSRM;
+
+/**
+ *
+ * @author zer0p1us
+ */
+public class Route {
+    public double duration;
+    public double distance;
+}
