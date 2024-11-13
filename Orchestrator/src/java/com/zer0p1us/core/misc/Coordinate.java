23a937dd2fe00b18ed6be1c43b736cb8949c5a41
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-11-13 19:25:52 +0000

    Added coordinates storage class

---
+++ Orchestrator/src/java/com/zer0p1us/core/misc/Coordinate.java
@@ -0,0 +1,15 @@
+package com.zer0p1us.core.misc;
+
+/**
+ *
+ * @author zer0p1us
+ */
+public class Coordinate {
+    public double longitude;
+    public double latitude;
+    
+    public Coordinate(double longitude, double latitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
