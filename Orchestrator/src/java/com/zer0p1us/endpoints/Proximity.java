09e05c4efaca51d8424b5a467cff7de6bcd7c4cd
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-12-21 20:39:58 +0000

    removed unused imports

--- Orchestrator/src/java/com/zer0p1us/endpoints/Proximity.java
+++ Orchestrator/src/java/com/zer0p1us/endpoints/Proximity.java
@@ -9,10 +9,8 @@ import java.net.URISyntaxException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
