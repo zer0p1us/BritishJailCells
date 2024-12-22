d8f34ec327382537e54106b9c33c434da57a7a59
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-12-22 22:32:43 +0000

    Implemented cancel endpoint

--- Orchestrator/src/java/com/zer0p1us/endpoints/Cancel.java
+++ Orchestrator/src/java/com/zer0p1us/endpoints/Cancel.java
@@ -1,12 +1,12 @@
 package com.zer0p1us.endpoints;
 
+import com.zer0p1us.core.Database;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.Produces;
-import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -32,6 +32,8 @@ public class Cancel {
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public void putJson(String content) {
+    public void putJson(@QueryParam("application_ref") String application_ref) {
+        Database db = new Database();
+        db.CancelApplication(application_ref);
     }
 }
