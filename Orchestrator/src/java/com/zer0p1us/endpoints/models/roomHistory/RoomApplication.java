e0daf85596bb994425ccf0e85547b6e516ab6ddb
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-12-22 20:47:44 +0000

    Added room application models

---
+++ Orchestrator/src/java/com/zer0p1us/endpoints/models/roomHistory/RoomApplication.java
@@ -0,0 +1,14 @@
+package com.zer0p1us.endpoints.models.roomHistory;
+
+/**
+ *
+ * @author zer0p1us <42619260+zer0p1us@users.noreply.github.com>
+ */
+public class RoomApplication {
+    
+    public String applicationRef;
+    public String roomId;
+    public String userId;
+    public String status;
+    
+}
---
+++ Orchestrator/src/java/com/zer0p1us/endpoints/models/roomHistory/RoomApplications.java
@@ -0,0 +1,9 @@
+package com.zer0p1us.endpoints.models.roomHistory;
+
+/**
+ *
+ * @author zer0p1us <42619260+zer0p1us@users.noreply.github.com>
+ */
+public class RoomApplications {
+    public RoomApplication[] applications;
+}
