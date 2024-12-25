d987e8764aac8ef9462d5e292cff6bf519acd896
zer01pus <42619260+zer0p1us@users.noreply.github.com>
2024-12-25 17:12:47 +0000

    Added `coordinates` and `weatherData` to `Room`

--- Orchestrator/src/java/com/zer0p1us/endpoints/models/rooms/Room.java
+++ Orchestrator/src/java/com/zer0p1us/endpoints/models/rooms/Room.java
@@ -1,5 +1,10 @@
 package com.zer0p1us.endpoints.models.rooms;
 
+import com.zer0p1us.endpoints.models.Coordinates;
+import com.zer0p1us.endpoints.models.WeatherData;
+import com.zer0p1us.service.GeoCodingService;
+import com.zer0p1us.service.WeatherService;
+
 /**
  *
  * @author zer0p1us
@@ -12,4 +17,18 @@ public class Room{
     public int price_per_month_gbp;
     public String availability_date;
     public String[] spoken_languages;
+    public Coordinates coordinates;
+    public WeatherData weatherData;
+
+    public Room fetchCoordinates() {
+        GeoCodingService geoService = GeoCodingService.getInstance();
+        this.coordinates = geoService.getCoordinatesFromPostcode(this.location.postcode.replaceAll(" ", ""));
+        return this;
+    }
+    
+    public Room fetchWeatherData() {
+        WeatherService weatherService = WeatherService.getInstance();
+        this.weatherData = weatherService.getWeather(this.coordinates);
+        return this;
+    }
 }
